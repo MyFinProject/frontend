9c9174af58ddfb38716ce2138b31f5df17b005c8
xK0NNANx <egoriksolo@gmail.com>
2025-04-22 18:48:03 +0300

    Feat: add db

--- Api/Models/Budget.cs
+++ Api/Models/Budget.cs
@@ -7,10 +7,12 @@ namespace Api.Models
         public int UserId { get; set; }
         public int CategoryId { get; set; }
         public int Amount { get; set; }
+        public int CurrencyId { get; set; }
         public string Period { get; set; } = string.Empty;
         public DateTime StartDate { get; set; } = DateTime.Now;
         public DateTime EndDate { get; set; }
         public User? User { get; set; }
         public Category? Category { get; set; }
+        public Currencie? Currencie { get; set; }
     }
 }
---
+++ Api/Models/Currencie.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Models
+{
+    public class Currencie
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public decimal Rate { get; set; }
+        public DateTime UpdateAt { get; set; } = DateTime.Now;
+        public List<UserWallets> UserAccounts { get; set; } = new List<UserWallets>();
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+        public List<Budget> Budgets { get; set; } = new List<Budget>();
+    }
+}
