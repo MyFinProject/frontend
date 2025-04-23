751a1d0386dd899229b0ed99b292c5950025274a
MrKik46 <MrKik46@mail.ru>
2025-04-24 01:07:53 +0300

    Начало вёрстки главной страницы

---
+++ src/components/balance_and_card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="top-row">
+      <div class="balance"></div>
+      <div class="card"></div>
+    </div>
+</template>
+
+<style scoped>
+.top-row {
+  margin-top: 10%;
+  display: flex;
+  justify-content: space-between;
+  gap: 5%;
+}
+
+.balance, .card {
+  width: 480px;
+  height: 180px;
+  background-color: white;
+  border-radius: 10px;
+}
+
+.card {
+  width: 480px;
+  background-color: rgb(41, 58, 43);
+}
+</style>
