b3a136d34b5880fbf58aea03ce1c84eb52be2613
aiviei <nastyabelyaeva1257@gmail.com>
2025-05-12 01:19:23 +0300

    добавление внешней части регистрации, входа, начало истории

---
+++ frontend/src/components/BodyHistory/BodyHistory.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="balance-back">
+        <div>
+            <span class="total-balance">Всего на счету: {{balance_value}} $</span>
+        </div>
+        <div class="balance-details">
+            <span class="balance-separately">Наличные: {{cash}} $</span>
+            <span class="balance-separately">На банковских картах: {{card}} $</span>
+            <span class="balance-separately">Вклады: {{contribution}} $</span>
+        </div>
+    </div>
+    <div class="">
+
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        balance_value: 1.5,
+        cash:  0.5,
+        card:  0.5,
+        contribution: 0.5
+      }
+    }
+  }
+</script>  
+
+<style>
+@import './body-history.css';
+</style>
--- frontend/src/components/RegistrationField/RegistrationField.vue
+++ frontend/src/components/RegistrationField/RegistrationField.vue
@@ -1,12 +1,66 @@
 <template>
     <button class="back-button" @click="$router.push('/')">назад</button>
-    <span class = 'MyFin'>MyFin</span>
-    <span class = "input-row-label">Регистрация</span>
-    <input class = 'input-row' type="text" placeholder="Введите имя пользователя">
-    <input class = 'input-row' type="text" placeholder="Введите почту">
-    <input class = 'input-row' type="text" placeholder="Введите пароль">
-    <button class="button-persona-account" @click="$router.push('/PersonalAccount')">лк</button>
+    <span class="MyFin">MyFin</span>
+    <span class="input-row-label">Регистрация</span>
+    <input v-model="username" class="input-row" type="text" placeholder="Введите имя пользователя">
+    <input v-model="email" class="input-row" type="text" placeholder="Введите почту">
+    <div v-if="passwordError" class="error-message">{{ passwordError }}</div>
+    <input v-model="password" class="input-row" type="password" placeholder="Введите пароль">
+    <button class="registration-end" @click="register">завершить регистрацию</button>
+    <div class="centered-row">
+        <span class="question">Есть аккаунт? →</span>
+        <button class="log-in-button" @click="$router.push('/LogIn')">Войти</button>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            email: '',
+            password: '',
+            passwordError: '' 
+        }
+    },
+    methods: {
+        validatePassword() {
+            if (this.password.length < 8) {
+                this.passwordError = 'Пароль должен содержать минимум 8 символов';
+                return false;
+            }
+            if (!/\d/.test(this.password)) {
+                this.passwordError = 'Пароль должен содержать хотя бы одну цифру';
+                return false;
+            }
+            
+            if (!/[A-ZА-Я]/.test(this.password)) {
+                this.passwordError = 'Пароль должен содержать хотя бы одну заглавную букву';
+                return false;
+            }
+
+            if (!/[a-zа-я]/.test(this.password)) {
+                this.passwordError = 'Пароль должен содержать хотя бы одну строчную букву';
+                return false;
+            }
+            
+            this.passwordError = '';
+            return true;
+        },
+        register() {
+            if (!this.validatePassword()) {
+                return; 
+            }
+            console.log('Регистрация:', {
+                username: this.username,
+                email: this.email,
+                password: this.password
+            });
+            this.$router.push('/PersonalAccount'); 
+        }
+    }
+}
+</script>
 
 <style scoped>
 @import './registration-field.css';
