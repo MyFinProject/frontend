cbc96c41116a9ccc68b0caa5c2d6f9c69a7fc27a
aiviei <nastyabelyaeva1257@gmail.com>
2025-05-18 02:07:46 +0300

    add box-shadow

--- frontend/src/components/Transfer/Transfer.vue
+++ frontend/src/components/Transfer/Transfer.vue
@@ -2,9 +2,11 @@
     <div class="transfer">
       <p class="future-goals-text">Ваши цели на будущее:</p>
       <div v-for="(item, index) in items" :key="index" class="input-row">
-        <span class = 'counter-goals'>{{ index + 1 }}.</span>
-        <input class = 'input-row' type="text" v-model="items[index].value" placeholder="Введите цель">
-        <button class="remove-button" @click="removeRow(index)" v-if="items.length > 1">×</button>
+      <div class="goal-container">
+          <span class = 'counter-goals'>{{ index + 1 }}.</span>
+          <input class = 'input-row' type="text" v-model="items[index].value" placeholder="Введите цель">
+          <button class="remove-button" @click="removeRow(index)" v-if="items.length > 1">×</button>
+        </div>
       </div>
     <button class = 'button-add' @click="addRow">Добавить цель</button>
     </div>
