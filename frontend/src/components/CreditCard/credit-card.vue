e0e474fa58ac198a416088c6d06822e6f0b9536e
aiviei <nastyabelyaeva1257@gmail.com>
2025-05-20 20:18:43 +0300

    redesigning 1

--- frontend/src/components/CreditCard/credit-card.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div class="credit-card"></div>
-</template>
--- frontend/src/pages/PersonalAccount/personal-account.vue
+++ frontend/src/pages/PersonalAccount/personal-account.vue
@@ -3,12 +3,16 @@
 import TopMenuPersonalAccount from '../../components/TopMenuPersonalAccount/top-menu-personal-account.vue';
 import BalanceCard from '../../components/BalanceCard/balance-card.vue';
 import Transfer from '../../components/Transfer/Transfer.vue';
+import Receipt from '../../components/ReceiptAdding/ReceiptAdding.vue';
 </script>
 
 <template>
     <div class="mybody">
       <TopMenuPersonalAccount />
-      <BalanceCard />
+      <div class="background">
+        <BalanceCard />
+        <Receipt />
+      </div>
       <Transfer />
     </div>
 </template>
